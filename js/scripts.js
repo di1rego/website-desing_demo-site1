d7341ed67c304b9dd529f6a79af875beb0c298b7
di1rego <112751337+di1rego@users.noreply.github.com>
2024-06-02 13:09:36 +0300

    02.06.2024
    Web-desing

---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+// card
+const showDialogBtn = document.querySelector('#showModal');
+const myDialog = document.querySelector('#modal');
+const closeDialogBtn = document.querySelector('#closeModal');
+
+showDialogBtn.addEventListener('click', () => {
+  myDialog.showModal();
+});
+
+closeDialogBtn.addEventListener('click', () => {
+  myDialog.close();
+});
+
+const showDialogBtn_2 = document.querySelector('#showModal_2');
+const myDialog_2 = document.querySelector('#modal_2');
+const closeDialogBtn_2 = document.querySelector('#closeModal_2');
+
+showDialogBtn_2.addEventListener('click', () => {
+  myDialog_2.showModal();
+});
+
+closeDialogBtn_2.addEventListener('click', () => {
+  myDialog_2.close();
+});
+
+const showDialogBtn_3 = document.querySelector('#showModal_3');
+const myDialog_3 = document.querySelector('#modal_3');
+const closeDialogBtn_3 = document.querySelector('#closeModal_3');
+
+showDialogBtn_3.addEventListener('click', () => {
+  myDialog_3.showModal();
+});
+
+closeDialogBtn_3.addEventListener('click', () => {
+  myDialog_3.close();
+});
+
+const showDialogBtn_4 = document.querySelector('#showModal_4');
+const myDialog_4 = document.querySelector('#modal_4');
+const closeDialogBtn_4 = document.querySelector('#closeModal_4');
+
+showDialogBtn_4.addEventListener('click', () => {
+  myDialog_4.showModal();
+});
+
+closeDialogBtn_4.addEventListener('click', () => {
+  myDialog_4.close();
+});
+
+const showDialogBtn_5 = document.querySelector('#showModal_5');
+const myDialog_5 = document.querySelector('#modal_5');
+const closeDialogBtn_5 = document.querySelector('#closeModal_5');
+
+showDialogBtn_5.addEventListener('click', () => {
+  myDialog_5.showModal();
+});
+
+closeDialogBtn_5.addEventListener('click', () => {
+  myDialog_5.close();
+});
+
+const showDialogBtn_6 = document.querySelector('#showModal_6');
+const myDialog_6 = document.querySelector('#modal_6');
+const closeDialogBtn_6 = document.querySelector('#closeModal_6');
+
+showDialogBtn_6.addEventListener('click', () => {
+  myDialog_6.showModal();
+});
+
+closeDialogBtn_6.addEventListener('click', () => {
+  myDialog_6.close();
+});
+
+// form
+const showDialogBtnForm = document.querySelector('#showModalForm');
+const myDialogForm = document.querySelector('#modalForm');
+const closeDialogBtnForm = document.querySelector('#closeModalForm');
+
+showDialogBtnForm.addEventListener('click', () => {
+  myDialogForm.showModal();
+});
+
+closeDialogBtnForm.addEventListener('click', () => {
+  myDialogForm.close();
+});
+
+const showDialogBtnForm_2 = document.querySelector('#showModalForm_2');
+const myDialogForm_2 = document.querySelector('#modalForm_2');
+const closeDialogBtnForm_2 = document.querySelector('#closeModalForm_2');
+
+showDialogBtnForm_2.addEventListener('click', () => {
+  myDialogForm_2.showModal();
+});
+
+closeDialogBtnForm_2.addEventListener('click', () => {
+  myDialogForm_2.close();
+});
+
+const showDialogBtnForm_3 = document.querySelector('#showModalForm_3');
+const myDialogForm_3 = document.querySelector('#modalForm_3');
+const closeDialogBtnForm_3 = document.querySelector('#closeModalForm_3');
+
+showDialogBtnForm_3.addEventListener('click', () => {
+  myDialogForm_3.showModal();
+});
+
+closeDialogBtnForm_3.addEventListener('click', () => {
+  myDialogForm_3.close();
+});
